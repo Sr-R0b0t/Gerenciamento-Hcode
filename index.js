9caf21247e08f7573ff2c1f87354f8adf4fc2ab6
Sr-R0b0t <fabinho.cristovam@gmail.com>
2024-09-02 13:05:47 -0300

    Manipulando o Dom

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("hello word")
+let nome = document.querySelector("#exampleInputName");
+let genero = document.querySelector("#exampleInputGenderM");
+let nascimento = document.querySelector("#exampleInputBirth");
+let pais = document.querySelector("#exampleInputCountry");
+let email = document.querySelector("#exampleInputEmail1");
+let senha = document.querySelector("#exampleInputPassword1");
