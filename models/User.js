3aec034e634cb309628cf0eb897cd157d9a3085b
Sr-R0b0t <fabinho.cristovam@gmail.com>
2024-09-10 22:54:49 -0300

    Atualização

---
+++ models/User.js
@@ -0,0 +1,18 @@
+//Toda classe criada no JS começa com letra Maiuscula ex: (User)
+class User {
+
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        //Atributo,  //variaveis
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+        
+    }
+
+}
